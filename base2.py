3d3cdb45e1ee7978b63fe5347399467a42057963
abiy-ahmed <cybercom276@gmail.com>
2019-12-05 21:38:02 -0600

    script

---
+++ base2.py
@@ -0,0 +1,11 @@
+num1 = float(input("Provide a number: "))
+base1 = float(input("Provide its base: "))
+base2 = float(input("Provide a new base: "))
+ex = float(0)
+product = ""
+if base1 == 10:
+    
+elif base1 != 10:
+    
+else base != 10:
+    print("The base is unknown.")
